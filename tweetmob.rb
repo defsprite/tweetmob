ec35e636b412ae323e955e409a486556776940bb
Christoph Sassenberg <christoph.sassenberg@googlemail.com>
2011-07-21 18:54:06 +0200

    agile design camp release. still embarassingly ugly.

--- tweetmob.rb
+++ tweetmob.rb
@@ -4,26 +4,23 @@ require 'erb'
 require 'twitter'
 require  './lib/stopwords'
 
-@@hashtag = "betahaushh"
-@@search = Twitter::Search.new.hashtag(@@hashtag).no_retweets.per_page(100)
-
 before do
   content_type :html, 'charset' => 'utf-8'
 end
 
-get '/' do
-  @words = get_frequencies
+get '/:hashtag' do
+  @words = get_frequencies(params[:hashtag])
   erb :tweets
 end
 
-def get_frequencies
-  text = @@search.fetch.map(&:text).flatten.join(" ")
-  text = text.tr(",'()","").gsub(/"|&quot;/,"")
+def get_frequencies(hashtag)
+  search = Twitter::Search.new.hashtag(hashtag).no_retweets.per_page(100)
+  text = search.fetch.map(&:text).flatten.join(" ")
+  text = text.gsub(/\.|,|;|\(|\)|"|&quot;|\?|\!|:|http:\/\/\S*|/,"")
   words = text.split(" ").reject {|word| Stopwords::LIST.has_key?(word.downcase) }
   freqs = Hash.new(0)
   words.each { |word| freqs[word.downcase] += 1 }
   freqs = freqs.sort_by { |x, y| y }
-  freqs.reject! {|k,v| k == "##{@@hashtag}"}
+  freqs.reject! {|k,v| k == "##{hashtag}"}
   freqs.reverse!
 end
-
